bad5ef68649f7f012c4a4a8127436ebe9599d4f1
Harshal <h512lf27@gmail.com>
2021-02-05 23:15:13 -0500

    Update MainActivity.java

--- 2nd_Grade_Literacy-master/app/src/main/java/com/example/demologin/MainActivity.java
+++ 2nd_Grade_Literacy-master/app/src/main/java/com/example/demologin/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //Load in main instance state
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
